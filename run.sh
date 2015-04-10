d266dca9ec26e327f396c6ef19a4a67ab7de4188
William Fleming <wmfleming@gmail.com>
2015-04-09 23:10:40 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo docker run -d -e "VIRTUAL_HOST=example.com" -e "MONGO_URL=mongodb://dbserver:27017/libreboard" \
-  -e "ROOT_URL=http://example.com" -p 5555:5555 -it miurahr/libreboard
+  -e "ROOT_URL=http://example.com" -p 5555:5555 -it wmfleming/libreboard
 DPID=`sudo docker ps -q -l`
 echo $DPID > /tmp/docker-libreboard.pid
 sudo mv /tmp/docker-libreboard.pid /run/
